4942c3a54aa6bd5579a2c030f94f73b50d2bc898
BrianABelt <54295621+BrianABelt@users.noreply.github.com>
2019-08-29 10:23:45 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,11 @@ int main(int argc, char **argv) {
 
 printf("Benjamin Finnegan\n");
 
-printf("Major: Computer Engineering\n", );
+printf("Major: Computer Engineering\n");
 
+printf("Brian Belt\n");
+  
+printf("Computer Engineering\n");
+  
 return 0;
 }
